ebc8a37ea16ab709ffd5a938fa29fcc705b98c16
jeya-prashanth <jeyaprashanth17@gmail.com>
2024-09-28 17:51:22 +0530

    This is Jeyaprashanth commiting

---
+++ Backend/config.js
@@ -0,0 +1,7 @@
+export const PORT=5555; 
+
+export const mongoDBURL='mongodb+srv://jeyaprashanth:prashanth17@books-store-mern.gxb5gru.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
+
+
+//Please create a free database fofr yourself.
+//This database will be  deleted after tutorial.
